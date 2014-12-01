541e0ac57ab3da33c879c8e25b5f0dd9f05e328c
Mark Grant <mark.alan.grant@gmail.com>
2014-12-01 02:54:52 -0600

    Move computation out of Main

--- elm/Display.elm
+++ elm/Display.elm
@@ -1,13 +1,15 @@
 module Display (display) where
-import LinearAlgebra (pcaEigenpairs)
+import LinearAlgebra (pcaEigenpairs, NonEmpty(..), mean)
 import Array as A
 import Dict as D
 import Model (..)
 import Util (pointToVect, vectToPoint)
 
-display : (Int,Int) -> State -> [Point] -> Point -> Element
-display (w,h) state eigenvectors mean =
+display : (Int,Int) -> State -> Element
+display (w,h) state =
   let points = state.points |> D.toList |> map snd
+      eigenpairs = getEigenpairs points
+      mean = getMean points
       w' = toFloat w
       h' = toFloat h
       styleAxis = traced (solid black)
@@ -15,31 +17,52 @@ display (w,h) state eigenvectors mean =
       yAxis = styleAxis <| segment (0, -h'/2) (0, h'/2)
       dots = map toDot points
       pointToVect (x,y) = A.fromList [x, y]
-      eigenArrows = drawEigenArrows mean eigenvectors
+      eigenArrows = drawEigenArrows mean eigenpairs
       projectedPoints = if state.projectionsVisible
-                        then project eigenvectors points
+                        then project eigenpairs points
                         else []
   in collage w h <| (xAxis::yAxis::dots) ++ eigenArrows ++ projectedPoints
 
-project : [Point] -> [Point] -> [Form]
-project es pts =
-  case es of
+project : [(Point,Float)] -> [Point] -> [Form]
+project pairs pts =
+  case pairs of
     [] -> []
-    (e::_) -> let proj (ex,ey) (x,y) = let n = ex*x + ey*y
-                                        in (n*ex,n*ey)
-                  toDot (x,y) = circle 5 |> filled blue |> move (x,y)
-               in map (proj e >> toDot) pts
+    (e,_)::_ ->
+      let proj (ex,ey) (x,y) = let n = ex*x + ey*y
+                               in (n*ex,n*ey)
+          toDot (x,y) = circle 5 |> filled blue |> move (x,y)
+      in map (proj e >> toDot) pts
 
-drawEigenArrows : Point -> [Point] -> [Form]
-drawEigenArrows (mx,my) pts =
-  let drawOne (x,y) = let seg = segment (mx,my) (mx+100*x, my+100*y) |> traced (solid red)
-                          dot = circle 5 |> filled red |> move (mx,my)
-                      in [seg, dot]
-  in pts |> map adjustQuadrant |> map drawOne |> concat
+drawEigenArrows : Point -> [(Point, Float)] -> [Form]
+drawEigenArrows (mx,my) pairs =
+  let drawOne ((x,y), l) =
+        let seg = segment (mx,my) (mx+100*x, my+100*y) |> traced (solid red)
+            dot = circle 5 |> filled red |> move (mx,my)
+        in [seg, dot]
+  in pairs |> map adjustQuadrant |> map drawOne |> concat
 
 toDot : Point -> Form
 toDot (x,y) = circle 5 |> filled black |> move (x,y)
 
-adjustQuadrant : Point -> Point
-adjustQuadrant (x,y) =
-  if x < 0 then  (-x,-y) else (x,y)
+adjustQuadrant : (Point, Float) -> (Point, Float)
+adjustQuadrant ((x,y), l) =
+  let p' = if x < 0 then  (-x,-y) else (x,y)
+  in (p',l)
+
+getMean : [Point] -> Point
+getMean pts =
+  case map pointToVect pts of
+    [] -> (0, 0)
+    (p::ps) -> NonEmpty p ps |> mean |> vectToPoint
+
+getEigenpairs : [Point] -> [(Point, Float)]
+getEigenpairs points =
+  let takeOne xs = case xs of
+                     [] -> []
+                     (b::bs) -> [b]
+  in points |> map pointToVect
+            |> pcaEigenpairs
+            |> sortBy snd
+            |> reverse
+            |> takeOne
+            |> map (\(x,y) -> (vectToPoint x, y))
--- elm/Main.elm
+++ elm/Main.elm
@@ -3,37 +3,11 @@ import Model (..)
 import Input (actions)
 import Display (display)
 import Update (update)
-import Numeric (..)
-import LinearAlgebra (..)
-import Util (..)
-import Array as A
-import Dict as D
 import Mouse
 import Window
-
-axes : Signal [Point]
-axes = let convert = map pointToVect >> pcaEigenpairs >> sortBy snd >> reverse >> map fst >> map vectToPoint
-           takeOne xs = case xs of
-                          [] -> []
-                          (b::bs) -> [b]
-       in points |> lift convert
-                 |> lift takeOne
-
-pointsMean : Signal Point
-pointsMean = let f pts = case map pointToVect pts of
-                           [] -> (0, 0)
-                           (p::ps) -> NonEmpty p ps |> mean |> vectToPoint
-             in lift f points
 
 state : Signal State
 state = foldp update initialState actions
 
-points : Signal [Point]
-points = let f st = st.points |> D.toList |> map snd
-         in lift f state
-
---main : Signal Element
-main = display <~ Window.dimensions
-                ~ state
-                ~ axes
-                ~ pointsMean
+main : Signal Element
+main = display <~ Window.dimensions ~ state
